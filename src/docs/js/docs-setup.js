4dcc254d4f3e4e9338f936b496295d211e6ee6db
Raymond <ray@firestitch.com>
2016-07-12 08:26:14 -0400

    commit

--- src/docs/js/docs-setup.js
+++ src/docs/js/docs-setup.js
@@ -1,35 +1,23 @@
 NG_DOCS={
   "sections": {
-    "api": "API Documentation",
-    "tutorial": "Tutorial"
+    "api": "API Documentation"
   },
   "pages": [
     {
       "section": "api",
-      "id": "app.controllers:ListerCtrl",
-      "shortName": "ListerCtrl",
-      "type": "interface",
-      "moduleName": "app",
-      "shortDescription": "A factory that allows easy access to the lister directive instance",
-      "keywords": "access allows api app controllers data directive easy factory instance interface lister load loading method triggers"
-    },
-    {
-      "section": "api",
-      "id": "app.directives:lister",
-      "shortName": "lister",
+      "id": "app.directives:fs-lister",
+      "shortName": "fs-lister",
       "type": "directive",
       "moduleName": "app",
-      "shortDescription": "",
-      "keywords": "$scope __all access actions api app appened applies array avoid binded box button call callback called class classname click clicked column columns configure contextual corresponds css custom data dataset default directive directives dropdown element entire equal events expecting fetch filter filterable filters function functions greater href html icon input interface item js key label length limit list lister listerinstance load ls-instance ls-options menu number object objects options paging paired parameter parameters passed perform places populate process properties query record records rendering ror row select selection sets side specifies starting supports table table-bordered table-striped text tile title total triggered type type-hint type-hint-array type-hint-function user values variables-matrix"
+      "keywords": "$event $scope __all access action actions adds api app appended appened applies array avoid binded box button call callback called cancel cancellabel checkbox class classname click clicked column columns configure configures confirm confirmation content contextual corresponds css custom data dataset default defines delete directive directives directly displayed dropdown element enables entire events expecting fetch filter filterable filters function functions greater html icon infinite inject injected input inserts interface item js key label left length limit list lister listerinstance load loads ls-instance ls-options menu menus message multiple norecords number object objects oklabel option options paging paired param parameter parameters passed perform places populate process properties query record records rendering resolve ror row rowclick scope scroll search second select selected selection sets side simular specifies starting supports table text tile title total triggered true type user values variable"
     }
   ],
   "apis": {
-    "api": true,
-    "tutorial": false
+    "api": true
   },
   "html5Mode": false,
   "editExample": true,
-  "startPage": "/",
+  "startPage": "/api",
   "scripts": [
     "angular.min.js"
   ]
